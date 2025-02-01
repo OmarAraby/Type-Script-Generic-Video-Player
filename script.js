1c15026982221fdedfe16735d174e1881fee07de
Omar Araby <omararaby789@gmail.com>
2025-02-01 09:49:33 +0200

    handle muted - progress - time -play logic

--- script.js
+++ script.js
@@ -37,6 +37,59 @@ class VideoPlayer {
     // don't forget to append it to player element
     this.player.appendChild(contrlsContainer);
   }
+  // after that i need to initialize element so i can manipulating and make my logic on  it
+
+  initializeElements() {
+    this.video = this.player.querySelector(".main-video");
+    this.controls = this.player.querySelector(".controls");
+    this.progressBar = this.player.querySelector(".progress-bar");
+    this.volumeSlider = this.player.querySelector(".volume_range");
+    this.volumeIcon = this.player.querySelector(".volume");
+    this.playPauseBtn = this.player.querySelector(".play_pause");
+    this.currentTime = this.player.querySelector(".current");
+    this.duration = this.player.querySelector(".duration");
+    this.fastForwardBtn = this.player.querySelector(".fast-forward");
+    this.fastRewindBtn = this.player.querySelector(".fast-rewind");
+    this.fullscreenBtn = this.player.querySelector(".fullscreen");
+  }
 
   // handle controls Logic
+  togflePlay() {
+    this.player.video.paused ? this.video.play() : this.video.pause();
+    this.playPauseBtn.innerHTML = this.video.paused ? "play-arrow" : "pause";
+  }
+
+  setVolume(value) {
+    const volume = value / 100;
+    this.video.volume = volume;
+    this.volumeSlider.value = value;
+    this.volumeIcon.textContent =
+      volume === 0 ? "volume_off" : volume < 0.4 ? "volume_down" : "volume_up";
+  }
+
+  toggleMute() {
+    this.video.muted = !this.video.muted;
+
+    if (this.video.muted) {
+      this.volumeSlider.value = 0;
+      this.volumeIcon.textContent = "volume_off";
+    } else {
+      this.volumeSlider.value = this.video.volume * 100;
+      this.volumeIcon.textContent =
+        this.video.volume < 0.4 ? "volume_down" : "volume_up";
+    }
+  }
+
+  updateProgress() {
+    const progress = (this.video.currentTime / this.video.duration) * 100;
+    this.progressBar.style.width = `${progress}%`;
+    this.currentTime.textContent = this.formatTime(this.video.currentTime);
+    console.log("time updated");
+  }
+
+  formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remaining = Math.floor(seconds % 60);
+    return `${minutes}:${remaining.toString().padStart(2, "0")}`;
+  }
 }
